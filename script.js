94e012fb94b0fb137e8e9ecd26772eec92508406
justasdap <justas_dapkevicius@hotmail.com>
2020-04-25 16:14:45 +0300

    added js subtract function which subtracts one number from the other

--- script.js
+++ script.js
@@ -6,22 +6,3 @@ function subtract(a, b){
     return a - b;
 }
 
-function multiply(a, b){
-    return a * b;
-}
-
-function divide(a, b){
-    return a / b;
-}
-
-function operate(ope, a, b){
-    if(ope === '+'){
-        add(a, b)
-    } else if(ope === '-'){
-        subtract(a, b);
-    } else if(ope === '*'){
-        multiply(a, b);
-    } else if(ope === '/'){
-        divide(a, b);
-    }
-}
