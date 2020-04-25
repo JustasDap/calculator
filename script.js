3ed1368ff3de8ab4fe689044c8f886cf3aef0f0f
justasdap <justas_dapkevicius@hotmail.com>
2020-04-25 21:34:29 +0300

    added event on buttons click, so far only numbers, dot, clear and backspace buttons have functions

--- script.js
+++ script.js
@@ -44,3 +44,75 @@ function backspace() {
     displayValue = displayValue.substring(0, displayValue.length - 1);
     show('');
 }
+
+document.getElementById('num1').addEventListener('click', function(){
+    show('1');
+})
+
+document.getElementById('num2').addEventListener('click', function(){
+    show('2');
+})
+
+document.getElementById('num3').addEventListener('click', function(){
+    show('3');
+})
+
+document.getElementById('num4').addEventListener('click', function(){
+    show('4');
+})
+
+document.getElementById('num5').addEventListener('click', function(){
+    show('5');
+})
+
+document.getElementById('num6').addEventListener('click', function(){
+    show('6');
+})
+
+document.getElementById('num7').addEventListener('click', function(){
+    show('7');
+})
+
+document.getElementById('num8').addEventListener('click', function(){
+    show('8');
+})
+
+document.getElementById('num9').addEventListener('click', function(){
+    show('9');
+})
+
+document.getElementById('num0').addEventListener('click', function(){
+    show('0');
+})
+
+document.getElementById('dot').addEventListener('click', function(){
+    show('.');
+})
+
+document.getElementById('equals').addEventListener('click', function(){
+    console.log('=');
+})
+
+document.getElementById('backspace').addEventListener('click', function(){
+    backspace();
+})
+
+document.getElementById('clear').addEventListener('click', function(){
+    clear();
+})
+
+document.getElementById('divide').addEventListener('click', function(){
+    console.log('/');
+})
+
+document.getElementById('multiply').addEventListener('click', function(){
+    console.log('*');
+})
+
+document.getElementById('minus').addEventListener('click', function(){
+    console.log('-');
+})
+
+document.getElementById('plus').addEventListener('click', function(){
+    console.log('+');
+})
